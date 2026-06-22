0394ed5d8c4462525fe76d413fbe79cc3bae698a
Ben Stabley <ben.stabley@nv5.com>
2026-06-22 14:00:10 -0700

    add separation between each file diff
    had to do some stupid crap to keep colors in terminal but not when putting diff into file

--- diff_atbx.ps1
+++ diff_atbx.ps1
@@ -3,7 +3,8 @@ function Diff-Atbx {
     param (
         [Parameter(Mandatory = $true)][Alias("A")][string]$Old,
         [Parameter(Mandatory = $true)][Alias("B")][string]$New,
-        [string]$TempDir = ".diffatbx_temp"
+        [string]$TempDir = ".diffatbx_temp",
+        [string]$Output
     )
         
     begin {
@@ -20,8 +21,18 @@ function Diff-Atbx {
             $NewDir = (Join-Path $TempDir "NEW")
             Unpack-Atbx -Name $New -Master $NewDir
 
+            # i don't like the -Output param (perfer > redirection) but neither of
+            # $Host.UI.RawUI.IsStdOutRedirected
+            # [System.Console]::IsOutputRedirected
+            # were able to detect when redirection was happening.
+
             # no-pager dumps entire diff to screen non-interactively
-            git --no-pager diff --no-index $OldDir $NewDir
+            if ($Output) {
+                (git --no-pager diff --no-index $OldDir $NewDir) -replace "diff --git", "`n`ndiff --git" > $Output
+            }
+            else {
+                (git --no-pager diff --color=always --no-index $OldDir $NewDir) -replace "diff --git", "`n`ndiff --git" | Write-Output
+            }
         }
         finally {
             # ctrl-c while in git diff pager kills script.
